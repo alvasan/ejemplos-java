d6ab33edb431500fdabf93794310ecc818e47232
fredStark <a.valdivieso11@gmail.com>
2017-04-15 15:19:49 -0500

    prueba servlet embedido

--- src/main/java/com/oaxacode/HelloWorldServlet.java
+++ src/main/java/com/oaxacode/HelloWorldServlet.java
@@ -17,9 +17,11 @@ public class HelloWorldServlet extends HttpServlet {
     }
 }*/
 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,8 +33,13 @@ import javax.servlet.http.HttpServletResponse;
 { "/test" })
 public class HelloWorldServlet extends HttpServlet
 {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        request.getRequestDispatcher("/WEB-INF/html/index.html").forward(request,response);
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        out.println( "Hello World Oaxacodes from Servlet3! embedded" );
+        out.flush();
+        out.close();
     }
 }
